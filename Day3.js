67ea58a7b3a32e82668c449a4d6993837ad8056f
Sabari7373 <115739400+Sabari7373@users.noreply.github.com>
2022-10-19 22:56:38 +0530

    Add files via upload

---
+++ Day3.js
@@ -0,0 +1,25 @@
+const resume =[{
+    "Name": "Sabari Arun G"
+},
+{
+    "phone":"6369133574",
+    "email":"sabariganesh7373@gmail.com",
+    "Educational Qualification " : " BSC Comuter science",
+    "Year of passing" : " 2018 ",
+    "University" : "Bharadhiar University"
+},
+{   "Work Exerience" : "Bpo",
+    "Job Role" : "Customer Care Executive ",
+    "Working Year" : "2018 to 2021",
+    "Company Name" : "Lessburn"
+},
+{
+    "Skills": "MS Word",
+    "hobbies": "traveling"
+},
+{
+    "Area of Intrest": "Full Stack Devoloper",
+    "conclution": " Thank You"
+}
+]
+ console.log(resume)
